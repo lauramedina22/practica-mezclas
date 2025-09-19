8158bfa521842dca97860ba1470e4108f728fd89
lauramedina22 <medinalauraisabella@gmail.com>
2025-09-19 08:10:48 -0500

    condicional

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ nombre = "Mundo"
 print("Hola, " + nombre + "!")
 print("Soy Lilith")
 print("Soy Laura")
+
+num1 = 5
+num2 = 10
+
+if num1 < num2:
+    print("num1 es menor que num2")
+else:
+    print("num1 no es menor que num2")
